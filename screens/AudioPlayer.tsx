467a60bd9b51c2d2cd6ce67191945ccb02e5237d
PupoSDC <pedro.pupo@greator.com>
2021-03-10 16:39:43 +0100

    Change audio playback library

--- screens/AudioPlayer.tsx
+++ screens/AudioPlayer.tsx
@@ -1,46 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button, View } from 'react-native';
 import { Text } from 'react-native';
-import TrackPlayer from 'react-native-track-player';
-
-const setup = async () => {
-    // Set up the player
-    await TrackPlayer.setupPlayer();
-
-    // Add a track to the queue
-    await TrackPlayer.add({
-        id: 'trackId',
-        url: require('../media/demoAudio.mp3'),
-        title: 'Uno',
-        artist: 'Pupito El Pedrito',
-        artwork: require('../media/demoAudioArtwork.jpeg')
-    });
-
-    await TrackPlayer.updateOptions({
-        capabilities: [
-          TrackPlayer.CAPABILITY_PLAY,
-          TrackPlayer.CAPABILITY_PAUSE,
-        ],
-        compactCapabilities: [
-          TrackPlayer.CAPABILITY_PLAY,
-          TrackPlayer.CAPABILITY_PAUSE
-        ]
-    });
-};
-
-const destroy = async () => {
-    await TrackPlayer.stop();
-    await TrackPlayer.destroy();
-}
+import { Player, Recorder, MediaStates } from '@react-native-community/audio-toolkit';
 
 export const AudioPlayerScreen = () => {
+    const playerRef = useRef<Player>();
     const [isPlaying, setIsPlaying] = useState(false);
-
+    
     useEffect(() => {
-        setup();
-        return () => {
-            destroy();
-        }
+        playerRef.current = new Player("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
+        return () => playerRef.current?.destroy();
     }, []);
 
     return (
@@ -50,9 +19,11 @@ export const AudioPlayerScreen = () => {
                 title={isPlaying ? "stop" : "play"}
                 onPress={() => setIsPlaying((isPlaying) => {
                     if (isPlaying) {
-                        TrackPlayer.stop();
+                        console.log("stop")
+                        playerRef.current?.pause();
                     } else {
-                        TrackPlayer.play();
+                        console.log("start")
+                        playerRef.current?.play();
                     }
                     return !isPlaying;
                 })}
